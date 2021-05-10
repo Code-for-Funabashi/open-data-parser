7d8d824f80e286829a5907b8537df706245a215a
nyk523jt@gmail.com <takubojunpei@Takubos-MacBook-Air.local>
2021-05-11 00:41:37 +0900

    deleted files; change keyname for details

--- open_data_parser/transformer.py
+++ open_data_parser/transformer.py
@@ -12,14 +12,14 @@ def transform(records: Iterable[Dict], output_schema: Set)->Iterable[Dict]:
     """
     transformed_records = []
     for r in records:
-        transformed_r = {"detailed":{}}
+        transformed_r = {"details":{}}
         # FIXME:
         # output_schemaで第一階層でkeyとして持つ情報以外は全てdetailedに入れちゃう仕様で作っている。
         # 表示する必要のない不要なカラム等をどこで制御する？frontendでやるべきでしょうか？
         for col in output_schema:
             if r.get(col):
                 transformed_r[col] = r.pop(col)
-        transformed_r["detailed"].update(r)
+        transformed_r["details"].update(r)
         transformed_records.append(transformed_r)
 
     return transformed_records
