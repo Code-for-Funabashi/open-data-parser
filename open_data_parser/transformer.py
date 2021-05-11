e4a843f88a9e92485601868de5a06550849da681
aca-ta <seljydef@gmail.com>
2021-05-11 22:09:30 +0900

    fix.

--- open_data_parser/transformer.py
+++ open_data_parser/transformer.py
@@ -1,24 +1,23 @@
-from dataclasses import dataclass
+"""transformer"""
+
+from typing import Dict
+from typing import Iterable
 import os
 
-import pandas as pd
 import googlemaps
 
 assert os.environ.get("GOOGLE_API_KEY"), "Set your GOOGLE_API_KEY."
 
 
-@dataclass(frozen=True)
-class Coordinate:
-    lat: float
-    lng: float
-
-
-def query_coordinate_from_address(address: str) -> Coordinate:
+def query_coordinate_from_address(data: Iterable[Dict]) -> Iterable[Dict]:
     """Query the coordinate from the address."""
     googleapikey = os.environ["GOOGLE_API_KEY"]
     gmaps = googlemaps.Client(key=googleapikey)
 
-    # TODO:
-    #  '山手1-3-17' / '本中山2-23-16'に大して、geocodeの返り値が[]になっていた。
-    # 一方、”船橋市”を先頭に付けることで返り値がNULLで無くなった。
-    return Coordinate(**gmaps.geocode(address)[0]["geometry"]["location"])
+    for record in data:
+        # TODO:
+        #  '山手1-3-17' / '本中山2-23-16'に大して、geocodeの返り値が[]になっていた。
+        # 一方、”船橋市”を先頭に付けることで返り値がNULLで無くなった。
+        lat, lng = gmaps.geocode(record["address"])[0]["geometry"]["location"]
+        record.update({"lat": lat, "lng": lng})
+        yield record
