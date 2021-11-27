64518cfd3642794d1b0a5815c844f2e05cdd62e6
j-takurou <nyk523jt@gmail.com>
2021-11-27 10:58:37 +0900

    不要なテスト削除

--- tests/open_data_parser/test_downloader.py
+++ tests/open_data_parser/test_downloader.py
@@ -1,36 +1,18 @@
 """ test for downloader.py """
 
 import unittest
-from unittest import mock
-from unittest.case import expectedFailure
-
 import open_data_parser.downloader as target
-
-# @mock.patch("request.urlopen")
-# class TestFetchShapefile(unittest.TestCase):
-#     def test_fetch_shapefile(self, client):
-#         """Test query_coordinate_from_address() queries on a combined address """
-
-#         data = [{"key1": "hoge", "key2": "fuga"}]
-
-#         result = target.fetch_shapefile(data, ["key1", "key2"])
-#         result.__next__()
-
-#         client.return_value.geocode.assert_called_once_with("hoge fuga")
-# unzip_shapefile
-
+import shapefile
 
 class TestUnzipShapefile(unittest.TestCase):
     def test_unzip_shapefile(self):
-        import shapefile
-
+        
         actual = target.unzip_shapefile(
             url="https://nlftp.mlit.go.jp/ksj/gml/data/A27/A27-10/A27-10_12_GML.zip",
             shp_fname="A27-10_12-g_SchoolDistrict.shp",
             dbf_fname="A27-10_12-g_SchoolDistrict.dbf",
         )
         self.assertIsInstance(actual, shapefile.ShapeRecords)
-
 
 class TestCombineKeys(unittest.TestCase):
     def test_combine_keys(self):
