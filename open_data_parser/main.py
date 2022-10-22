d7d2a53ca90c2e24d7d2e77d31f36c5f779eba20
aca-ta <seljydef@gmail.com>
2022-10-22 16:53:20 +0900

    2022年12月データを反映 (#25)

--- open_data_parser/main.py
+++ open_data_parser/main.py
@@ -65,8 +65,8 @@ TARGETS = [
                 "acceptable_5yo",
             ),
             skiprows=9,
-            skipfooter=4, # 位置情報が公開されていないため、家庭的保育事業者はスキップ
-            usecols="E,H:M",
+            skipfooter=3, # 位置情報が公開されていないため、家庭的保育事業者はスキップ
+            usecols="F,I:N",
         ),
         transformers=[
             partial(
